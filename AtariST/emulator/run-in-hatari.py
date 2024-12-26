a7e48ddbd2d00bd259c6b16405336876f28f83a8
Philip Zembrod <pzembrod@gmail.com>
2024-12-26 10:13:57 +0100

    Terminate hatari driven by makefile done.txt in Forth

--- AtariST/emulator/run-in-hatari.py
+++ AtariST/emulator/run-in-hatari.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
 
-import sys
+import os, sys, time
 
 sys.path.append('/usr/share/hatari/hconsole')
 
 import hconsole
 
-args = ['--mono', '--cpuclock', '32', '--auto', 'C:\\4THCORE.PRG',
-        '--confirm-quit', 'false', '.']
+args = ['hatari', '--mono', '--cpuclock', '32', # '--conout', '2',
+        '--auto', 'C:\\4THCORE.PRG', '--confirm-quit', 'false', '.']
 
 print('will create main')
 main = hconsole.Main(args)
 code = hconsole.Scancode
 print('main created')
 
-main.run('sleep 10')
-print('3 slept')
+while not main.tokens.hatari.is_running():
+  print('waiting for hatari to run')
+  time.sleep(1)
+
+time.sleep(7)
+
+main.run('text makefile done.txt')
+main.run('keypress %s' % code.Return)
+
+while main.tokens.hatari.is_running() and not os.path.exists('DONE.TXT'):
+  print('waiting for done.txt')
+  time.sleep(1)
+print('done.txt found')
+
 main.run("kill")
 print('killed')
